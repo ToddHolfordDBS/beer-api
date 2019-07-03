cf4689e6113f11c000eb55449e497b0174385573
toddholford <todd.holford@daugherty.com>
2019-07-03 15:54:31 -0500

    Added more beer

--- routes/beerRouter.js
+++ routes/beerRouter.js
@@ -1,8 +1,43 @@
 const express = require('express');
 const beerRouter = express.Router();
+const Beer = require('../models/beer')
+
+beerRouter.post('/', (req, res) =>{
+    console.log(req.body.name)
+    let beer = new Beer();
+    beer.name = req.body.name;
+    beer.rating = req.body.rating;
+    beer.save((err, beer) => {
+        if (err) {
+            res.status(400).send(err);
+        } else {
+            res.send(`Saved: ${beer}`)
+        }
+    });
+});
+
+beerRouter.get('/', (req, res) => {
+    Beer.find((err, beers) => {
+        if (err) {
+            res.send(err)
+        } else {
+            res.json(beers)
+        }
+    })
+})
+
+beerRouter.get('/:beer_id', (req, res) => {
+    Beer.findById(req.params.beer_id, (err, beer) => {
+        if (err) {
+            res.send(err)
+        } else {
+            res.json(beer)
+        }
+    })
+})
 
 beerRouter.use('/', (req,res) => {
     res.send('Beer router working!')
-})
+});
 
 module.exports = beerRouter;
--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose')
 const app = express();
 const beerRouter = require('./routes/beerRouter')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 app.use('/beers', beerRouter);
 
 app.use('/', (req, res) => {
     res.send('You hit the base route!');
+})
+
+mongoose.connect('mongodb://localhost:27017/beers', {
+    useNewUrlParser: true
+})
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to beers database')
+})
+
+mongoose.connection.on('error', (error) => {
+    console.error('Error connecting to database:\n${error}')
 })
 
 const port = process.env.PORT || 9999
